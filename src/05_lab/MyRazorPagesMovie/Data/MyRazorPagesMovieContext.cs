44c5737ac66fad3acc84346c1cd121a6cf3f2668
adrianmazur-dev <adrian.mazur@microsoft.wsei.edu.pl>
2025-04-13 14:15:48 +0200

    Microsoft.Learn

---
+++ src/05_lab/MyRazorPagesMovie/Data/MyRazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyRazorPagesMovie.Models;
+
+namespace MyRazorPagesMovie.Data
+{
+    public class MyRazorPagesMovieContext : DbContext
+    {
+        public MyRazorPagesMovieContext (DbContextOptions<MyRazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyRazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
