2c3f9cbd6d5e2c8c1369233bbcf2656d27b414af
adrianmazur-dev <adrian.mazur@microsoft.wsei.edu.pl>
2025-04-13 13:40:03 +0200

    Testowanie generowania i użycia tokena JWT

--- src/04_lab/JwtWebApiProject/Program.cs
+++ src/04_lab/JwtWebApiProject/Program.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
+using WebApi.Domain.Security;
 using WebApi.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WSEI Web API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter into field the word 'Bearer' following by space and JWT",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+   {
+     new OpenApiSecurityScheme
+     {
+       Reference = new OpenApiReference
+       {
+         Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"
+       }
+      },
+      new string[] { }
+    }
+  });
+});
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -26,6 +52,9 @@ builder.Services.AddAuthentication(options =>
     };
 });
 builder.Services.AddAuthorization();
+
+var jwtSection = builder.Configuration.GetSection("Jwt");
+builder.Services.Configure<JwtConfig>(jwtSection);
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
